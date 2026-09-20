f3a2b6bff649dd26b7d7287d2d4badae8d94e88b
chent5 <chen2015tong@gmail.com>
2026-09-20 16:08:10 +0200

    add performance gc

---
+++ performance/garbage-collection/test/GCCollectObjectsTest.java
@@ -0,0 +1,8 @@
+
+public class GCCollectObjectsTest {
+
+    public static void main(String[] args) {
+        GCCollectObjects collectObjects = new GCCollectObjects();
+        collectObjects.testGcCollectObject();
+    }
+}
